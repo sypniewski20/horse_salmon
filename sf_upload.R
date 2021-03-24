6b7cf9e97e69112e303f7ccf212cf814d8bc8b17
Mateusz Sypniewski <sypniewski20@gmail.com>
2021-03-24 12:35:06 +0100

    Add files via upload

---
+++ sf_upload.R
@@ -0,0 +1,27 @@
+### This script reads .sf files from analysis with salmon and joins them into single dataframe
+
+library(tidyverse)
+setwd('~/rnaseq/')
+
+### Creating list of .sf files
+sfl <- list.files(recursive = T)
+sfl2 <- sfl[grep('*.sf',sfl)]
+fl <- list.files('RNA-Seq/SALMON_OUT/')
+
+rnadf <- list()
+
+### Appending .sf files into list
+for (i in 1:length(sfl2)) {
+  df <- read.table(sfl2[i], header = T)
+  df$tissue <- fl[i]
+  rnadf[[i]] <- df
+}
+
+### Binding files into single dataframe
+dfinal <- bind_rows(rnadf)
+
+### Removing rnadf for memory saving
+remove(rnadf)
+
+### Saving dataframe to RDS
+saveRDS(dfinal,'sfbind')
